38bce18f5ca36e36ecbb214c206be7c1f0f91692
j2noo <wlsdndml213@gmail.com>
2024-07-23 15:44:37 +0900

    feat : spring project init (CC-83)

---
+++ src/main/java/ai/softeer/caecae/CaecaeApplication.java
@@ -0,0 +1,13 @@
+package ai.softeer.caecae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaecaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaecaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/softeer/caecae/CaecaeApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.softeer.caecae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaecaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
