a89c110f4f549f309cc2beb572ef8cd1888c3c62
Jinwoo Park <wlsdndml213@gmail.com>
2024-07-29 18:30:33 +0900

    feat :  S3 이미지 업로드 api 생성, IAM role을 활용한 ec2 권한 부여 (CC-90) (#6)
    * chore : 환경변수 관리를 위한 S3.yml을 깃 이그노어
    
    * chore : 환경변수 관리를 위한 S3.yml을 깃 이그노어
    
    * chore : 환경변수 관리를 위한 S3.yml을 깃 이그노어 (CC-90)
    
    * chore : 환경변수 관리를 위한 S3.yml을 깃 이그노어 (CC-90)
    
    * feat : 멀티파트 형태의 이미지를 받아 S3에 업로드하는 로직 및 컨트롤러 생성 (CC-90)
    
    * refactor : IAM role을 사용하여 ec2에 s3 접근 권한을 부여하도록 로직 수정 (CC-90)

---
+++ src/main/java/ai/softeer/caecae/S3Controller.java
@@ -0,0 +1,27 @@
+package ai.softeer.caecae;
+
+import ai.softeer.caecae.global.utils.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+public class S3Controller {
+    private final S3Service s3Service;
+
+    @PostMapping("api/s3")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        String filePath = s3Service.uploadFile(file);
+        return filePath +"created!";
+        //TODO : ResponseEntity 생성하기
+
+    }
+
+    @PostMapping("/api/health")
+    public String healthCheck() {
+        return "Hello Casper!";
+    }
+}
---
+++ src/main/java/ai/softeer/caecae/global/utils/S3Service.java
@@ -0,0 +1,63 @@
+package ai.softeer.caecae.global.utils;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException; // 추가
+import java.io.InputStream; // 추가
+import java.util.UUID; // 추가
+
+@Slf4j
+@Service
+//TODO : JPA 의존성 추가 후 @Transcational
+@RequiredArgsConstructor
+public class S3Service {
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    /**
+     * S3에 파일을 업로드 하는 서비스 로직
+     * @param file
+     * @return 파일 이름
+     */
+    //TODO : JPA 의존성 추가 후 @Transcational
+    public String uploadFile(MultipartFile file) {
+        String fileName = createFileName(file.getOriginalFilename());
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+        String filePath;
+        // 파일 업로드
+        try (InputStream inputStream = file.getInputStream()) {
+            amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+            // 올린 오브젝트에 대한 s3 url
+             filePath = amazonS3.getUrl(bucket, fileName).toString();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("파일이 없습니다.");
+            //TODO : 커스텀 에러 관리하기
+        }
+        log.info(filePath," is successfully created in S3");
+        return filePath;
+    }
+
+    // caecae+UUID 로 파일 이름 생성하기
+    private String createFileName(String fileName) {
+        return "caecae-" + UUID.randomUUID().toString().concat(getFileExtension(fileName));
+    }
+
+    // 파일 확장자 추출하기
+    private String getFileExtension(String fileName) {
+        log.info("fileName : {}", fileName.substring(fileName.lastIndexOf(".")));
+        return fileName.substring(fileName.lastIndexOf("."));
+    }
+}
