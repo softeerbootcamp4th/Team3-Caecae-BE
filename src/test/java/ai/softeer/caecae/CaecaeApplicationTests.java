9b9a3cc34973fc5535c5d9e5f63354b1e66c1f31
j2noo <wlsdndml213@gmail.com>
2024-07-30 17:33:06 +0900

    fix : 테스트코드 주석 제거  (CC-90)

--- src/test/java/ai/softeer/caecae/CaecaeApplicationTests.java
+++ src/test/java/ai/softeer/caecae/CaecaeApplicationTests.java
@@ -1,13 +1,13 @@
-//package ai.softeer.caecae;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class CaecaeApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package ai.softeer.caecae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaecaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
