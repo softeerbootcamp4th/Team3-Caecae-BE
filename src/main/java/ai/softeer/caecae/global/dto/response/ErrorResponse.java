76dc7e85cc9e5a54072b1ef6854dc912ed8e3df4
j2noo <wlsdndml213@gmail.com>
2024-07-30 15:26:16 +0900

    feat : Response 객체에 @getter 추가  (CC-96)

--- src/main/java/ai/softeer/caecae/global/dto/response/ErrorResponse.java
+++ src/main/java/ai/softeer/caecae/global/dto/response/ErrorResponse.java
@@ -1,6 +1,8 @@
 package ai.softeer.caecae.global.dto.response;
 
+import lombok.Getter;
 
+@Getter
 public class ErrorResponse extends BaseResponse{
     public ErrorResponse(int responseCode, String message) {
         super(responseCode, message);
