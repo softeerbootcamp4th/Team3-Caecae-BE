0cf483c460e08f7a9f46e01b98572f23272642b3
j2noo <wlsdndml213@gmail.com>
2024-07-31 03:03:05 +0900

    chore : parameter store 주석처리 해제  (CC-90)

--- src/test/java/ai/softeer/caecae/CaecaeApplicationTests.java
+++ src/test/java/ai/softeer/caecae/CaecaeApplicationTests.java
@@ -3,7 +3,6 @@ package ai.softeer.caecae;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @Disabled
 @SpringBootTest
