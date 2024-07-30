a0d19af15ce8c7ed6d312bf804a54095648b8e0b
j2noo <wlsdndml213@gmail.com>
2024-07-31 02:49:57 +0900

    test : 테스트 비활성화  (CC-90)

--- src/test/java/ai/softeer/caecae/CaecaeApplicationTests.java
+++ src/test/java/ai/softeer/caecae/CaecaeApplicationTests.java
@@ -1,8 +1,11 @@
 package ai.softeer.caecae;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@Disabled
 @SpringBootTest
 class CaecaeApplicationTests {
 
