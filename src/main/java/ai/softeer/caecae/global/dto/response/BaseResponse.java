204f776e61044fab4c19d6d8a74ca52ccacbb208
j2noo <wlsdndml213@gmail.com>
2024-08-04 05:13:36 +0900

    feat : Http요청에 관한 응답 본문에 담을 객체 생성(CC-114)

--- src/main/java/ai/softeer/caecae/global/dto/response/BaseResponse.java
+++ src/main/java/ai/softeer/caecae/global/dto/response/BaseResponse.java
@@ -3,10 +3,14 @@ package ai.softeer.caecae.global.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Http 요청에 대한 응답(성공,실패)시 본문에 반환할 객체
+ */
 @Getter
 @AllArgsConstructor
 public abstract class BaseResponse {
     // 커스텀 응답 코드 종류
+    // TODO : 커스텀 응답코드 문서화
     private int responseCode;
     // 응답 정보를 담은 메시지
     private String message;
