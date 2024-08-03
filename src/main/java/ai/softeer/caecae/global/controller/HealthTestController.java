0f8efe015026307a5d26d246fbb1b47cb283309e
j2noo <wlsdndml213@gmail.com>
2024-08-04 05:14:57 +0900

    refactor : health check api에서 SuccessResponse를 사용하도록 수정 (CC-114)

--- src/main/java/ai/softeer/caecae/global/controller/HealthTestController.java
+++ src/main/java/ai/softeer/caecae/global/controller/HealthTestController.java
@@ -1,6 +1,7 @@
 package ai.softeer.caecae.global.controller;
 
 import ai.softeer.caecae.global.dto.response.SuccessResponse;
+import ai.softeer.caecae.global.enums.SuccessCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HealthTestController {
     @GetMapping("/api/health")
     public ResponseEntity<SuccessResponse<String>> healthTest() {
-        return ResponseEntity.ok(new SuccessResponse<>(1000,"api health test에 성공했습니다.","health test v1"));
+        return SuccessResponse.of(SuccessCode.OK, "health test v1");
+
     }
 
 }
