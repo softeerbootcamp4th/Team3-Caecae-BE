4fac3cb40eeb69d896291ba87caa4bb439d747b8
j2noo <wlsdndml213@gmail.com>
2024-08-04 05:25:22 +0900

    feat : RacingGame 도메인에서 발생하는 에러만을 처리하는 로직 생성 (CC-114)

---
+++ src/main/java/ai/softeer/caecae/racinggame/domain/exception/RacingGameException.java
@@ -0,0 +1,17 @@
+package ai.softeer.caecae.racinggame.domain.exception;
+
+import ai.softeer.caecae.global.enums.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class RacingGameException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public RacingGameException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+
+
+    }
+
+}
