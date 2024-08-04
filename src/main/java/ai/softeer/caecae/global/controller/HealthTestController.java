d177aae74e3ad6a90a8dc52a709c79a30174a953
j2noo <wlsdndml213@gmail.com>
2024-08-04 16:08:44 +0900

    refactor : @Controller 대신 @RestController를 사용 (CC-114)

--- src/main/java/ai/softeer/caecae/global/controller/HealthTestController.java
+++ src/main/java/ai/softeer/caecae/global/controller/HealthTestController.java
@@ -3,16 +3,14 @@ package ai.softeer.caecae.global.controller;
 import ai.softeer.caecae.global.dto.response.SuccessResponse;
 import ai.softeer.caecae.global.enums.SuccessCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HealthTestController {
     @GetMapping("/api/health")
     public ResponseEntity<SuccessResponse<String>> healthTest() {
-        return SuccessResponse.of(SuccessCode.OK, "health test v1");
-
+        return SuccessResponse.of(SuccessCode.OK,"Caecae Spring Server Health Test ~!");
     }
-
 }
 
