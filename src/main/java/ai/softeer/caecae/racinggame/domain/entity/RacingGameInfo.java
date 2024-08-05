d3fa91e95c0b9b66fe542d77253fcd0c533ee2fe
j2noo <wlsdndml213@gmail.com>
2024-08-05 18:07:29 +0900

    fix : LocalDateTime 직렬화/역직렬화를 위한 코드 추가 (CC-118)

--- src/main/java/ai/softeer/caecae/racinggame/domain/entity/RacingGameInfo.java
+++ src/main/java/ai/softeer/caecae/racinggame/domain/entity/RacingGameInfo.java
@@ -1,33 +1,37 @@
 package ai.softeer.caecae.racinggame.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.Column;
-import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.data.redis.core.RedisHash;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@RedisHash(value = "racingGameInfo") // redis 에 저장될 키 값, TTL 은 설정하지 않음
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RacingGameInfo {
-    @Id
-    private final String id;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     @Column(nullable = false)
     private LocalDateTime startTime;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     @Column(nullable = false)
     private LocalDateTime endTime;
 
     @Column(nullable = false)
     private int numberOfWinners;
 
-    @Builder
-    public RacingGameInfo(LocalDateTime startTime, LocalDateTime endTime, int numberOfWinners) {
-        this.id = "racingGameInfo"; // 고정된 id 값 설정
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.numberOfWinners = numberOfWinners;
-    }
 }
