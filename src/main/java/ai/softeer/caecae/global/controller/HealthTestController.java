5889a1006bf0250b92374684649cef329f6db7c5
j2noo <wlsdndml213@gmail.com>
2024-08-05 18:29:23 +0900

    feat : healthtest cors (hotfix/cors)

--- src/main/java/ai/softeer/caecae/global/controller/HealthTestController.java
+++ src/main/java/ai/softeer/caecae/global/controller/HealthTestController.java
@@ -3,14 +3,16 @@ package ai.softeer.caecae.global.controller;
 import ai.softeer.caecae.global.dto.response.SuccessResponse;
 import ai.softeer.caecae.global.enums.SuccessCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HealthTestController {
     @GetMapping("/api/health")
     public ResponseEntity<SuccessResponse<String>> healthTest() {
-        return SuccessResponse.of(SuccessCode.OK, "Caecae Spring Server Health Test ~!");
+        return SuccessResponse.of(SuccessCode.OK, "Caecae Spring Server Health Test ~! with cors");
     }
 }
 
