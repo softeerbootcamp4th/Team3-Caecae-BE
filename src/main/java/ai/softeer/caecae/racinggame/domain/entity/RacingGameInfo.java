1a63e157bdea5bcbfbce32e824794a9a5bc73688
j2noo <wlsdndml213@gmail.com>
2024-08-05 16:40:29 +0900

    feat : @RedisHash를 사용하여 레디스에 저장하는 엔티티 로직 (CC-118)

--- src/main/java/ai/softeer/caecae/racinggame/domain/entity/RacingGameInfo.java
+++ src/main/java/ai/softeer/caecae/racinggame/domain/entity/RacingGameInfo.java
@@ -1,15 +1,18 @@
 package ai.softeer.caecae.racinggame.domain.entity;
 
-import ai.softeer.caecae.global.entity.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.time.LocalDateTime;
 
-@Entity
-public class RacingGameInfo extends BaseEntity {
+@Getter
+@RedisHash(value = "racingGameInfo") // redis 에 저장될 키 값, TTL 은 설정하지 않음
+public class RacingGameInfo {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private final String id;
 
     @Column(nullable = false)
     private LocalDateTime startTime;
@@ -19,4 +22,12 @@ public class RacingGameInfo extends BaseEntity {
 
     @Column(nullable = false)
     private int numberOfWinners;
+
+    @Builder
+    public RacingGameInfo(LocalDateTime startTime, LocalDateTime endTime, int numberOfWinners) {
+        this.id = "racingGameInfo"; // 고정된 id 값 설정
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.numberOfWinners = numberOfWinners;
+    }
 }
