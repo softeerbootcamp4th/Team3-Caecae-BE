cb37016464bfa8339bb35ed8238d34378d09e710
j2noo <wlsdndml213@gmail.com>
2024-08-07 00:25:18 +0900

    rename : 레이싱게임 정보 api, controller 통합  (CC-120)

--- src/main/java/ai/softeer/caecae/racinggame/repository/RacingGameParticipantRepository.java
+++ src/main/java/ai/softeer/caecae/racinggame/repository/RacingGameParticipantRepository.java
@@ -2,7 +2,9 @@ package ai.softeer.caecae.racinggame.repository;
 
 import ai.softeer.caecae.racinggame.domain.entity.RacingGameParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RacingGameParticipantRepository extends JpaRepository<RacingGameParticipant, Integer> {
 
 }
