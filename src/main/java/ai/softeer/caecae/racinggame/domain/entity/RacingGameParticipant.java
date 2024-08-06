bb4a3d64636b50ceb95fcb4d972e0cf6388d3d94
j2noo <wlsdndml213@gmail.com>
2024-08-06 18:20:55 +0900

    feat : 레이싱게임 참여자 엔티티에 컬럼 업데이트 메서드 추가  (CC-120)

--- src/main/java/ai/softeer/caecae/racinggame/domain/entity/RacingGameParticipant.java
+++ src/main/java/ai/softeer/caecae/racinggame/domain/entity/RacingGameParticipant.java
@@ -3,8 +3,13 @@ package ai.softeer.caecae.racinggame.domain.entity;
 import ai.softeer.caecae.global.entity.BaseEntity;
 import ai.softeer.caecae.user.domain.entity.User;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class RacingGameParticipant extends BaseEntity {
     @Id
     private Integer userId;
@@ -17,4 +22,19 @@ public class RacingGameParticipant extends BaseEntity {
     private double distance;
 
     private Integer selection;
+
+    // 315km 와의 차이를 반환하는 메서드
+    public double getAbsoluteDistance() {
+        return Math.abs(315 - distance);
+    }
+
+    // 커스텀 옵션이 선택되었는지 여부를 반환하는 메서드
+    public Boolean isOptionSelected() {
+        return selection != null;
+    }
+
+    // 엔티티의 점수(거리)를 업데이트하는 메서드
+    public void updateDistance(double distance) {
+        this.distance = distance;
+    }
 }
