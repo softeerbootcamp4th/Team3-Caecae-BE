24c69842d5b296a04dda6c4264bfcbb37b1f4734
j2noo <wlsdndml213@gmail.com>
2024-08-06 18:21:16 +0900

    feat : 레이싱게임에 참여하거나, 점수를 업데이트하는 로직 추가  (CC-120)

---
+++ src/main/java/ai/softeer/caecae/racinggame/service/RacingGameParticipantService.java
@@ -0,0 +1,70 @@
+package ai.softeer.caecae.racinggame.service;
+
+import ai.softeer.caecae.racinggame.domain.dto.request.RegisterRacingGameResultRequestDto;
+import ai.softeer.caecae.racinggame.domain.dto.response.RegisterRacingGameResultResponseDto;
+import ai.softeer.caecae.racinggame.domain.entity.RacingGameParticipant;
+import ai.softeer.caecae.racinggame.repository.RacingGameParticipantRepository;
+import ai.softeer.caecae.user.domain.dto.request.FindAndRegisterUserRequestDto;
+import ai.softeer.caecae.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RacingGameParticipantService {
+    private final UserService userService;
+    private final RacingGameParticipantRepository racingGameParticipantRepository;
+
+    /**
+     * 사용자가 레이싱게임 결과를 등록하는 서비스 로직
+     *
+     * @param req
+     * @return
+     */
+    public RegisterRacingGameResultResponseDto registerOrUpdateRacingGameResult(RegisterRacingGameResultRequestDto req) {
+        // UserRepository 에서 사용자를 조회, 없으면 등록하고 해당 유저의 userId를 반환
+        Integer userId = userService.findAndRegisterUser(FindAndRegisterUserRequestDto.builder()
+                .phone(req.phone())
+                .build()).userId();
+
+        // 찾은 userId 로 레이싱게임에 참여했는지 찾기
+        Optional<RacingGameParticipant> optionalRacingGameParticipant = racingGameParticipantRepository.findById(userId);
+
+        // 레이싱게임 참여 이력이 존재하지 않음
+        if (optionalRacingGameParticipant.isEmpty()) {
+            registerRacingGameResult(req);
+            return RegisterRacingGameResultResponseDto.builder()
+                    .isOptionSelected(false)
+                    .build();
+        }
+
+        // 레이싱게임 참여 이력이 존재함
+        RacingGameParticipant racingGameParticipant = optionalRacingGameParticipant.get();
+
+        // 점수가 더 높은경우 (차이가 더 작은 경우) 데이터를 갱신함
+        // TODO : 315를 constant화
+        if (racingGameParticipant.getAbsoluteDistance() > Math.abs(315 - req.distance())) {
+            racingGameParticipant.updateDistance(req.distance());
+            racingGameParticipantRepository.save(racingGameParticipant);
+        }
+
+        return RegisterRacingGameResultResponseDto.builder()
+                .isOptionSelected(racingGameParticipant.isOptionSelected())
+                .build();
+    }
+
+    /**
+     * 사용자가 레이싱게임결과를 신규로 등록하는 로직
+     *
+     * @param req 전화번호, 레이싱게임의 결과 distance
+     * @return 등록된 레이싱게임결과 엔티티
+     */
+    private RacingGameParticipant registerRacingGameResult(RegisterRacingGameResultRequestDto req) {
+        RacingGameParticipant racingGameParticipant = RacingGameParticipant.builder()
+                .distance(req.distance())
+                .build();
+        return racingGameParticipantRepository.save(racingGameParticipant);
+    }
+}
