fad8e9d58a3762c3af7585e477650669815df98f
j2noo <wlsdndml213@gmail.com>
2024-08-07 16:47:46 +0900

    feat : 유저를 찾지 못하는 에러코드 추가 (CC-120)

--- src/main/java/ai/softeer/caecae/global/enums/ErrorCode.java
+++ src/main/java/ai/softeer/caecae/global/enums/ErrorCode.java
@@ -18,10 +18,15 @@ public enum ErrorCode implements BaseCode {
     NOT_FOUND(-1000, "잘못된 요청입니다.", HttpStatus.NOT_FOUND),
 
     /**
-     * 2xxx : 레이싱게임 ..
+     * 2xxx : 레이싱게임
      */
     NEED_AUTHENICATE(1000, "권한이 필요한 요청입니다,", HttpStatus.UNAUTHORIZED),
     RACING_GAME_NOT_FOUND(2000, "등록된 레이싱게임이 없습니다.", HttpStatus.NOT_FOUND),
+
+    /**
+     * 3xxx : 유저
+     */
+    USER_NOT_FOUND(-3000, "해당 휴대폰 번호로 유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 
 
     /**
