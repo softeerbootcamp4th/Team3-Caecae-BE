4afec9ac7778e670b94034cb3412f1fa993ef9b4
j2noo <wlsdndml213@gmail.com>
2024-08-07 15:19:45 +0900

    fix : 두 개의 엔티티가 연관되어 있으므로 영속성 유지를 위해 트랜잭션 저장 (CC-120)

--- src/main/java/ai/softeer/caecae/racinggame/service/RacingGameParticipantService.java
+++ src/main/java/ai/softeer/caecae/racinggame/service/RacingGameParticipantService.java
@@ -4,12 +4,12 @@ import ai.softeer.caecae.racinggame.domain.dto.request.RegisterRacingGameResultR
 import ai.softeer.caecae.racinggame.domain.dto.response.RegisterRacingGameResultResponseDto;
 import ai.softeer.caecae.racinggame.domain.entity.RacingGameParticipant;
 import ai.softeer.caecae.racinggame.repository.RacingGameParticipantRepository;
-import ai.softeer.caecae.user.domain.dto.request.FindAndRegisterUserRequestDto;
 import ai.softeer.caecae.user.domain.entity.User;
-import ai.softeer.caecae.user.service.UserService;
+import ai.softeer.caecae.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -17,7 +17,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class RacingGameParticipantService {
-    private final UserService userService;
+    private final UserRepository userRepository;
     private final RacingGameParticipantRepository racingGameParticipantRepository;
 
     /**
@@ -26,11 +26,10 @@ public class RacingGameParticipantService {
      * @param req
      * @return
      */
+    @Transactional // 복합키를 가지는 두 개의 엔티티를 하나의 영속성에서 저장하기 위해 트랜잭션
     public RegisterRacingGameResultResponseDto registerOrUpdateRacingGameResult(RegisterRacingGameResultRequestDto req) {
-        // UserRepository 에서 사용자를 조회, 없으면 등록하고 해당 유저의 userId를 반환
-        User user = userService.findAndRegisterUser(FindAndRegisterUserRequestDto.builder()
-                .phone(req.phone())
-                .build());
+        // phone 으로 유저를 조회하고, 존재하지 않으면 유저를 등록하여 반환하기
+        User user = findOrRegisterUser(req.phone());
 
         // 찾은 userId 로 레이싱게임에 참여했는지 찾기
         Optional<RacingGameParticipant> optionalRacingGameParticipant = racingGameParticipantRepository.findById(user.getId());
@@ -66,12 +65,38 @@ public class RacingGameParticipantService {
      * @param req 전화번호, 레이싱게임의 결과 distance
      * @return 등록된 레이싱게임결과 엔티티
      */
-
     private RacingGameParticipant registerRacingGameResult(RegisterRacingGameResultRequestDto req, User user) {
         RacingGameParticipant racingGameParticipant = RacingGameParticipant.builder()
                 .distance(req.distance())
                 .user(user)
                 .build();
         return racingGameParticipantRepository.save(racingGameParticipant);
+    }
+
+
+    /**
+     * 휴대폰 번호로 유저 조회하고, 존재하지 않으면 유저를 등록하여 반환하는 내부 로직
+     *
+     * @param phone 휴대폰 번호
+     * @return 저장된 유저 엔티티
+     */
+    private User findOrRegisterUser(String phone) {
+        Optional<User> optionalUser = userRepository.findByPhone(phone);
+        return optionalUser.orElseGet(() -> registerUser(phone));
+    }
+
+    /**
+     * 휴대폰 번호로 유저를 등록하는 내부 로직
+     *
+     * @param phone 휴대폰 번호
+     * @return 저장된 유저 엔티티
+     */
+    private User registerUser(String phone) {
+        User user = User.builder()
+                .phone(phone)
+                .build();
+        return userRepository.save(user);
+
+
     }
 }
