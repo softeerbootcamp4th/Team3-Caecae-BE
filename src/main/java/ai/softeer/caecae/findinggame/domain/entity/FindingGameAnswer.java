2c06005b23fcb68aaeb647ea6cea9a5d01de424a
j2noo <wlsdndml213@gmail.com>
2024-08-09 10:52:45 +0900

    feat: 숨은그림찾기 정답 정보 bulk insert를 위한 엔티티 생성 전략 수정 (CC-129)

--- src/main/java/ai/softeer/caecae/findinggame/domain/entity/FindingGameAnswer.java
+++ src/main/java/ai/softeer/caecae/findinggame/domain/entity/FindingGameAnswer.java
@@ -2,11 +2,16 @@ package ai.softeer.caecae.findinggame.domain.entity;
 
 import ai.softeer.caecae.global.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.*;
 
+@Builder
+@Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FindingGameAnswer extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
 
     @Column(nullable = false)
@@ -27,4 +32,12 @@ public class FindingGameAnswer extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "finding_game_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private FindingGame findingGame;
+
+    public void updateFindingGame(int coordX, int coordY, String descriptionImageUrl, String title, String content) {
+        this.coordX = coordX;
+        this.coordY = coordY;
+        this.descriptionImageUrl = descriptionImageUrl;
+        this.title = title;
+        this.content = content;
+    }
 }
