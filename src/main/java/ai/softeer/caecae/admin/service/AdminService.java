7184ef9b759ce4b02d6ea82826653780b2cb5fd3
j2noo <wlsdndml213@gmail.com>
2024-08-09 11:03:19 +0900

    feat : 여러번의 저장 로직이 하나의 트랜잭션 내에서 수행되도록 어노테이션 추가 (CC-129)

--- src/main/java/ai/softeer/caecae/admin/service/AdminService.java
+++ src/main/java/ai/softeer/caecae/admin/service/AdminService.java
@@ -90,6 +90,7 @@ public class AdminService {
      * @param req
      * @return
      */
+    @Transactional
     public FindingGameDailyAnswerResponseDto saveFindingGameDailyAnswer
     (FindingGameDailyAnswerRequestDto req) {
 
